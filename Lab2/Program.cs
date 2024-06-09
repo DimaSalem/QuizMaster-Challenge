9bfb8be8988a827dea82323ca52219028bb23c87
Dima Salem <demamohamad1998@gmail.com>
2024-06-09 18:08:48 +0300

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizMasterChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
